385006c3cee3f8c0fbc60be5ed31effce41ddc63
007vict <super_vg@bk.ru>
2019-12-18 19:54:23 +0300

    change url in email.send

--- blog/tasks.py
+++ blog/tasks.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import, unicode_literals
+
+from urllib import request
+
 from celery import task, shared_task
 from django.core.mail import send_mail
 
@@ -10,5 +13,5 @@ def post_create(id):
     if posts.author.followers.all:
         for user in posts.author.followers.all():
             subject = 'New post "{}"'.format(posts.title)
-            message = 'Dear {},\n\nYou have successfully a new post "{}" - http://127.0.0.1:8000{} for author {}.'.format(user.username, posts.title, posts.get_absolute_url(), posts.author)
+            message = 'Dear {},\n\nYou have successfully a new post "{}" - http://{}:1337{} for author {}.'.format(user.username, posts.title, request.localhost(), posts.get_absolute_url(), posts.author)
             send_mail(subject, message, 'super_vg@bk.ru', [user.email])
